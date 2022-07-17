04428ed5f38dd294bc4ad047c65daeb8506598e6
baddi <baddi@MacBook-Pro-van-pc.local>
2022-07-17 17:49:24 +0100

    add example 1

---
+++ Java/Example_1/src/com/company/main/HelloDate.java
@@ -0,0 +1,10 @@
+package com.company.main;
+
+import java.util.Date;
+
+public class HelloDate {
+    public static void main(String[] args) {
+        System.out.println("Hello, it's: ");
+        System.out.println(new Date());
+    }
+}

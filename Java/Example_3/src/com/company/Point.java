3ee64aedb69a8099a90c9c84c7a3b9b83ec3cee1
baddi <baddi@MacBook-Pro-van-pc.local>
2022-07-18 09:41:47 +0100

    add example 3

---
+++ Java/Example_3/src/com/company/Point.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Point {
+    double abs;
+    double ord;
+
+    public double getAbs() {
+        return abs;
+    }
+
+    public void setAbs(double abs) {
+        this.abs = abs;
+    }
+
+    public double getOrd() {
+        return ord;
+    }
+
+    public void setOrd(double ord) {
+        this.ord = ord;
+    }
+}
